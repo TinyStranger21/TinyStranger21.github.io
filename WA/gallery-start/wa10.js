9aa30b3bdccee1f42101228c467e337cd1817170
Kai Franks <123043443+TinyStranger21@users.noreply.github.com>
2023-03-24 18:01:07 -0600

    Worked on wa10 and edited Index css

---
+++ WA/gallery-start/wa10.js
@@ -0,0 +1,38 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+
+/* Declaring the alternative text for each image file */
+const alts = ['Closeup of a human eye', 'picture of rock', 'picture of pansees', 'Eqyptian heroglyphs', 'Picture of butterfly']
+
+/* Looping through images */
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', `${alts[image]}`);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', eve => {
+        displayedImage.src = eve.target.src;
+        displayedImage.alt = eve.target.alt;
+    })
+}
+
+/* Wiring up the Darken/Lighten button */
+// const btnClass = btn.getAttribute('class')
+btn.addEventListener('click', d => {
+    const btnClass = btn.getAttribute('class')
+    if (btnClass === 'dark') {
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+      } else {
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      }
+});
