c2ac320184b47312d908b6be7e02829ca353a6f0
Kai Franks <123043443+TinyStranger21@users.noreply.github.com>
2023-03-26 11:45:03 -0600

    WA10 finished and index css edited

--- WA/gallery-start/wa10.js
+++ WA/gallery-start/wa10.js
@@ -5,10 +5,12 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+// const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+const images = ['myPic1.jpg', 'myPic2.JPG', 'myPic3.JPG', 'myPic4.jpg', 'myPic5.JPG']
 
 /* Declaring the alternative text for each image file */
-const alts = ['Closeup of a human eye', 'picture of rock', 'picture of pansees', 'Eqyptian heroglyphs', 'Picture of butterfly']
+// const alts = ['Closeup of a human eye', 'picture of rock', 'picture of pansees', 'Eqyptian heroglyphs', 'Picture of butterfly']
+const alts = ['Jar of Lights', 'Close up of Kais Face', 'Pic of Kai in Front of Lights', 'Orange Kai in Store', 'Pondering Life at Lake Kai']
 
 /* Looping through images */
 for (const image of images) {
