ee80ebaffc4360bb3edfef8ba3eb02fd774d6390
Kai Franks <123043443+TinyStranger21@users.noreply.github.com>
2023-04-23 20:06:47 -0600

    wa13 completed

--- WA/wa13.js
+++ WA/wa13.js
@@ -1,88 +1,20 @@
+// New Idea: I am going to make a small box with a huge scroll amount and have a random position generator for each number of volume 1-100. 
+// To do that part we will add an append that will append or createElement--for i in range(100), adding to a div we have in the html.
 
-
-// function check() {
-//     console.log('test');
-// }
-
-// function submit() {
-//     alert("Your volume is now:" + output.textContent);
-// }
-
-// function reset() {
-//     outputInt = 0;
-//     output.textContent = outputInt;
-// }
-
-// function minus() {
-//     if (outputInt > 0) {
-//     outputInt -=1;
-//     output.textContent = outputInt; }
-    
-// }
-
-// function plus() {
-//     if (outputInt < 100) {
-//     outputInt +=1;
-//     output.textContent = outputInt;
-//     }
-// }
-
-// function random() {
-//     outputInt = randomNumber(0, 100);
-//     output.textContent = outputInt;
-// }
-
-// function randomNumber(min, max) {
-//     const num = Math.floor(Math.random() * (max - min + 1)) + min;
-//     return num;
-//   }
-
-
-
-// const output = document.querySelector('.output');
-// let outputInt = parseInt(output.textContent);
-// console.log(outputInt);
-
-// const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-// const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
-// const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-// const randomButton = document.querySelector('.random-button').addEventListener('click', random);
-// const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
-
-//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-/* const button = document.querySelector('.button');
-const output = document.querySelector('.output');
-let phone_content = document.querySelector('.phone');
-button.addEventListener('click', updateOutput);
-function updateOutput() {
-    output.textContent = phone_content.value;
-    alert(phone_content.value);
-}
-*///!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
-// var slider = document.getElementById("myRange");
-// var sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
-// var sliderOutput = document.querySelector(".slider-output");
-
-
-// // Update the current slider value (each time you drag the slider handle)
-// function update() {
-//     sliderOutput.textContent = slider.value;
-// }
-
-let body = document.querySelector("body");
-let content = document.querySelector("#content");
-let volume = document.querySelector("#volume");
+const body = document.querySelector("body");
+const content = document.querySelector("#content");
+// let volume = document.querySelector("#volume");
+const numbers = document.querySelector("#numbers");
+// const myDiv = document.querySelector("#myDiv");
 
 // Slider box tracking JavaScript
 // currently 15 colors
 const colors = ["#00BBF9", "#FFA9A3", "Yellow", "Orange", "#00F5D4", "Pink", "#E7D7C1", "#CEFF1A", "#B9E6FF", "#7EB77F", "Black", "#59A96A", "#9BDEAC", "#A9F8FB", "#FF4365"];
 console.log(colors.length);
-console.log(570/colors.length);
+console.log(3788/colors.length);
 // 38*15=570
 function myFunction() {
-    const element = document.getElementById("myDIV");
+    const element = document.getElementById("myDiv");
     let x = element.scrollLeft;
     let y = element.scrollTop;
     document.getElementById ("demo").innerHTML = "Horizontally: " + x.toFixed() + "<br>Vertically: " + y.toFixed();
@@ -91,33 +23,29 @@ function myFunction() {
     colorChanger(numIndexer(x));
     boxChanger(numIndexer(y));
     // volume()
-    addition(x);
-    addition(y);
-    subtraction(x);
-    subtraction(y);
-    division(x);
-    division(y);
-
     // console.log(x); //x is the value we will assign
     // rgbChanger(x, y);
+  
+    // createParagraph();
+
+    // console.log()
+    
 }
 
 // Need to make a volume number that will change depending on the other functions
-function volume(value) {
-    volume.textContent = `Volume: ${value}`;
-}
+// function volume(value) {
+//     volume.textContent = `Volume: ${value}`;
+// }
 
 // getattribute
-
+// Box 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function numIndexer(x) {
-    let ind = Math.floor(x / 38);
+    let ind = Math.floor(x / 252);
     return ind;
 }
-
-// could use switch statements
 // Could also change the value of an rgb when the x and y values change ----- rgb (x,y,x+y)!!!!!!!!!!!!!!!!!!!!!!!!!!
 // maybe I could divide the thing by 15 and then set a color to each individual value like 1, 2, 3, 4, ...
-// Box 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 function colorChanger(numInd) {
     body.style.backgroundColor = colors[numInd]; // This should change as 
 }
@@ -132,30 +60,53 @@ function boxChanger(numInd) {
 // }
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
+function createButton() {
+    for (i=0; i<=100; i++) {
 
-function addition(n) {
-    if (value <= 100) {
-        value += n;
-        return value;
+        // const btn = document.querySelector("#numbers");
+
+        const btn = document.createElement("button");
+        btn.textContent = i;
+        btn.classList.add("numb");
+        document.getElementById("myDiv").appendChild(btn);
+        randomPos()
+        btn.style.top = randomPos() + "px";
+        btn.style.left = randomPos() + "px";
+        // btn.addEventListener('click', volumeChange); // Bring volume change function into our addEventListener 
+        btn.addEventListener('click', () => {
+            const volume = document.querySelector("#volume");
+            volume.textContent = `Volume: ${event.target.textContent}`;// btn.textContent;
+            console.log(volume); 
+        })
+
     }
 }
 
-function subtraction(n) { // we could also put x and y in here
-    if (value <= 100) {
-        value -= n;
-        return value;
-    }
-}
+// function volumeChange() {
+//     const volume = document.querySelector("#volume");
+//     volume.textContent = "Volume: " + event.target.textContent;// btn.textContent;
+//     console.log(volume); 
+// }
 
-function division(n) {
-    if (value <= 100) {
-        value /= n;
-        return value;
-    }
+function randomPos() {
+    const random = Math.floor(Math.random()*3940);
+    return random; 
 }
+console.log(createButton());
 
-function backChanger() {
+// const numButton = document.querySelector('.num-button').addEventListener('click', "A function that gives the value of the number to the volume");
 
-}
+// numbers.style.top = randomPos(); // Assigning a random number amount to a makeshift x and y axis
+// numbers.style.left = randomPos();
+
+// for (i=0; i<=100; i++) {
+//     randomPos();
+//     // console.log(randomPos());
+// }
 
 // make a scroll that if you hit 50 it automatically restarts the page and the volume
+
+// Here is what I am trying to do:
+// I am trying to generate a bunch of buttons using javascript that are each a number from 1-100
+// Each of these buttons will also have a random position value assigned to it.
+// My problem currently is figuring out how to add a bunch of buttons to my html with the slider box.
--- WA/wa13try1.js
+++ WA/wa13try1.js
@@ -71,25 +71,18 @@ function updateOutput() {
 //     sliderOutput.textContent = slider.value;
 // }
 
-// New Idea: I am going to make a small box with a huge scroll amount and have a random position generator for each number of volume 1-100. 
-// To do that part we will add an append that will append or createElement--for i in range(100), adding to a div we have in the html.
-
-
-
 let body = document.querySelector("body");
 let content = document.querySelector("#content");
-// let volume = document.querySelector("#volume");
-let numbers = document.querySelector("#numbers");
-// const myDiv = document.querySelector("#myDiv");
+let volume = document.querySelector("#volume");
 
 // Slider box tracking JavaScript
 // currently 15 colors
 const colors = ["#00BBF9", "#FFA9A3", "Yellow", "Orange", "#00F5D4", "Pink", "#E7D7C1", "#CEFF1A", "#B9E6FF", "#7EB77F", "Black", "#59A96A", "#9BDEAC", "#A9F8FB", "#FF4365"];
 console.log(colors.length);
-console.log(3788/colors.length);
+console.log(570/colors.length);
 // 38*15=570
 function myFunction() {
-    const element = document.getElementById("myDiv");
+    const element = document.getElementById("myDIV");
     let x = element.scrollLeft;
     let y = element.scrollTop;
     document.getElementById ("demo").innerHTML = "Horizontally: " + x.toFixed() + "<br>Vertically: " + y.toFixed();
@@ -98,29 +91,33 @@ function myFunction() {
     colorChanger(numIndexer(x));
     boxChanger(numIndexer(y));
     // volume()
+    addition(x);
+    addition(y);
+    subtraction(x);
+    subtraction(y);
+    division(x);
+    division(y);
+
     // console.log(x); //x is the value we will assign
     // rgbChanger(x, y);
-  
-    // createParagraph();
-
-    // console.log()
-    
 }
 
 // Need to make a volume number that will change depending on the other functions
-// function volume(value) {
-//     volume.textContent = `Volume: ${value}`;
-// }
+function volume(value) {
+    volume.textContent = `Volume: ${value}`;
+}
 
 // getattribute
-// Box 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 function numIndexer(x) {
-    let ind = Math.floor(x / 252);
+    let ind = Math.floor(x / 38);
     return ind;
 }
+
+// could use switch statements
 // Could also change the value of an rgb when the x and y values change ----- rgb (x,y,x+y)!!!!!!!!!!!!!!!!!!!!!!!!!!
 // maybe I could divide the thing by 15 and then set a color to each individual value like 1, 2, 3, 4, ...
-
+// Box 1 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function colorChanger(numInd) {
     body.style.backgroundColor = colors[numInd]; // This should change as 
 }
@@ -135,53 +132,30 @@ function boxChanger(numInd) {
 // }
 //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-function createButton() {
-    for (i=0; i<=100; i++) {
 
-        // const btn = document.querySelector("#numbers");
-
-        const btn = document.createElement("button");
-        btn.textContent = i;
-        btn.classList.add("numb");
-        document.getElementById("myDiv").appendChild(btn);
-        randomPos()
-        btn.style.top = randomPos() + "px";
-        btn.style.left = randomPos() + "px";
-        // btn.addEventListener('click', volumeChange); // Bring volume change function into our addEventListener 
-        btn.addEventListener('click', () => {
-            const volume = document.querySelector("#volume");
-            volume.textContent = `Volume: ${event.target.textContent}`;// btn.textContent;
-            console.log(volume); 
-        })
-
+function addition(n) {
+    if (value <= 100) {
+        value += n;
+        return value;
     }
 }
 
-// function volumeChange() {
-//     const volume = document.querySelector("#volume");
-//     volume.textContent = "Volume: " + event.target.textContent;// btn.textContent;
-//     console.log(volume); 
-// }
-
-function randomPos() {
-    const random = Math.floor(Math.random()*3940);
-    return random; 
+function subtraction(n) { // we could also put x and y in here
+    if (value <= 100) {
+        value -= n;
+        return value;
+    }
 }
-console.log(createButton());
 
-// const numButton = document.querySelector('.num-button').addEventListener('click', "A function that gives the value of the number to the volume");
+function division(n) {
+    if (value <= 100) {
+        value /= n;
+        return value;
+    }
+}
 
-// numbers.style.top = randomPos(); // Assigning a random number amount to a makeshift x and y axis
-// numbers.style.left = randomPos();
+function backChanger() {
 
-// for (i=0; i<=100; i++) {
-//     randomPos();
-//     // console.log(randomPos());
-// }
+}
 
 // make a scroll that if you hit 50 it automatically restarts the page and the volume
-
-// Here is what I am trying to do:
-// I am trying to generate a bunch of buttons using javascript that are each a number from 1-100
-// Each of these buttons will also have a random position value assigned to it.
-// My problem currently is figuring out how to add a bunch of buttons to my html with the slider box.
